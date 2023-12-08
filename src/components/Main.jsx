156a71ffc5629a7bfa322c3fc5c03303212de5ed
Bruno Prestes <brunoasprestes@gmail.com>
2023-12-07 22:58:46 -0300

    Ajuste no título

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,11 @@ const Main = () => {
   return (
     <div id="main">
       {/* Backgroud hero */}
-      <div className="relative w-full bg-slate-300">
+      <div className="relative w-full">
         <img src={bgMain} className="w-full h-screen object-cover" alt="/" />
 
         {/* Container main */}
-        <div className="w-full h-screen absolute top-0 left-0 bg-white/50 justify-center">
+        <div className="w-full h-screen absolute top-0 left-0 bg-gradient-to-r from-slate-100 from-3% via-slate-300 justify-center">
           <h1 className="font-bold text-4xl m-5">Villa CoWorking</h1>
 
           <div className="max-w-[850px] m-auto h-96 px-5 w-full absolute bottom-36 md:bottom-24">
@@ -20,10 +20,9 @@ const Main = () => {
                 profissionalmente e ter liberdade
               </h1>
               <h2 className="text-base mt-2 md:mt-5 md:text-xl font-bold">
-                Reduza custos com espaços de coworking e promova seu
-                crescimento profissional. Escolha entre diferentes espaços de
-                trabalho e tenha acesso a recursos de infraestrutura de
-                qualidade.
+                Reduza custos com espaços de coworking e promova seu crescimento
+                profissional. Escolha entre diferentes espaços de trabalho e
+                tenha acesso a recursos de infraestrutura de qualidade.
               </h2>
             </div>
 
@@ -40,7 +39,7 @@ const Main = () => {
             </div>
           </div>
 
-          <div className="absolute bottom-2 left-24 md:bottom-4 md:right-4 flex flex-col md:flex-row md:mt-5 justify-center md:justify-end items-center">
+          <div className="absolute bottom-2 left-24 md:bottom-4 md:left-5 flex flex-col md:flex-row md:mt-5 justify-center md:justify-end items-center">
             <div className="text-center">
               <h2 className="text-sm md:text-lg">Área de cobertura</h2>
               <h2 className="font-bold">Macapá e Santana</h2>
