1b40d240c3241bea545559404ba7dad8b656cd88
Bruno Prestes <brunoasprestes@gmail.com>
2023-12-08 18:12:21 -0300

    Add icons section beneficios

--- src/components/Beneficios.jsx
+++ src/components/Beneficios.jsx
@@ -1,50 +1,63 @@
 import React from "react";
 import BeneficiosItem from "./BeneficiosItem";
+import { FaInternetExplorer, FaUsersRectangle, FaMapLocationDot } from "react-icons/fa6"
+import { IoBusiness } from "react-icons/io5";
+import { SiGotomeeting } from "react-icons/si";
+import { MdOutlineCastForEducation, MdChair, MdCleanHands, MdSentimentSatisfiedAlt } from "react-icons/md";
 
 const Beneficios = () => {
   const beneficios = [
     {
       id: 1,
+      icon: <FaInternetExplorer />,
       title: "Internet estável e rápida",
       desc: "Temos uma estrutura de internet super rápida. Internet lenta ou instável vai impactar na sua produtividade ou do seu time. Não fique sem internet no meio dos seus trabalhos, eventos ou reuniões. Livre-se dos custos com internet problemática e que lhe deixa na mão sempre quando você mais precisa.",
     },
     {
       id: 2,
+      icon: <FaUsersRectangle />,
       title: "Ambiente Colaborativo",
       desc: "Proporcionamos um ambiente colaborativo para que você feche novos negócios e troque experiêcias com outras empresas ou profissionais interagindo no dia a dia em um coworking, seja em nossos ambientes compartilhados ou eventos realizados dentro do Villa Coworking.",
     },
     {
       id: 3,
+      icon: <IoBusiness />,
       title: "Flexibilidade e adaptação",
       desc: "Tenha planos e espaços flexíveis que se ajustam ao crescimento da sua empresa, permitindo aumentar o espaço conforme a necessidade da sua empresa, sem as limitações, custos inicias e despesas fixas ao alugar um novo escritório tradicional.",
     },
     {
       id: 4,
+      icon: <SiGotomeeting />,
       title: "Espaços para reuniões com privacidade",
       desc: "Espaços modernos, confortáveis e com privacidade garantida para realização de reuniões da sua empresa.",
     },
     {
       id: 5,
+      icon: <FaMapLocationDot />,
       title: "Localização e acessibilidade estratégica",
       desc: "Estamos localizados estratégicamente com fácil acesso a transporte público, estacionamento conveniente e proximidade a áreas comerciais ou hubs empresariais.",
     },
     {
       id: 6,
+      icon: <MdChair />,
       title: "Móveis premium de alta qualidade e confortáveis",
       desc: "Oferecemos espaços com móveis premium e confortáveis para sua equipe e clientes.",
     },
     {
       id: 7,
+      icon: <MdCleanHands />,
       title: "Ambiente sempre limpo e agradável",
       desc: "Livre-se do ônus de preocupar-se com a limpeza do seu ambiente empresarial. Foque no seu negócio e deixe essas tarefas cotidianas conosco. 😊.",
     },
     {
       id: 8,
+      icon: <MdOutlineCastForEducation />,
       title: "Realização de eventos de networking, workshops, mentorias",
       desc: "Estamos sempre realizando eventos de diversos tipos para nossos membros, onde terá a oportunidade de fazer networking.",
     },
     {
       id: 9,
+      icon: <MdSentimentSatisfiedAlt />,
       title: "Valorização da satisfação do cliente",
       desc: "Oferecemos um atendimento ao cliente eficiente, suporte técnico ágil e a capacidade de resolver problemas prontamente.",
     },
@@ -79,7 +92,7 @@ const Beneficios = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {beneficios.map((e) => {
-            return <BeneficiosItem id={e.id} title={e.title} desc={e.desc} />;
+            return <BeneficiosItem id={e.id} icon={e.icon} title={e.title} desc={e.desc} />;
           })}
         </div>
       </div>
--- src/components/BeneficiosItem.jsx
+++ src/components/BeneficiosItem.jsx
@@ -1,8 +1,14 @@
-const BeneficiosItem = ({ id, title, desc }) => {
+const BeneficiosItem = ({ id, icon, title, desc }) => {
   return (
-    <div key={id} className="w-full sm:p-5 md:flex md:flex-col p-5 bg-gradient-to-l from-gray-700">
+    <div
+      key={id}
+      className="w-full sm:p-5 md:flex md:flex-col p-5 bg-gradient-to-l from-gray-700"
+    >
+      <div className="text-4xl text-white flex justify-center mb-3">{icon}</div>
       <h2 className="text-white font-bold text-2xl mb-3">{title}</h2>
-      <h2 className="text-stone-200 text-base leading-5 text-justify">{desc}</h2>
+      <h2 className="text-stone-200 text-base leading-5 text-justify">
+        {desc}
+      </h2>
     </div>
   );
 };
