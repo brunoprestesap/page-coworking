e7c6f83f79b75351c337f5f7a4a4ed5a4c75e901
Bruno Prestes <brunoasprestes@gmail.com>
2023-12-08 22:05:35 -0300

    Ajustes nas secoes ofertas e satisfacao

--- src/components/Oferta.jsx
+++ src/components/Oferta.jsx
@@ -3,16 +3,16 @@ import { FaWhatsapp } from "react-icons/fa6";
 
 const Oferta = () => {
   return (
-    <div className="p-5">
+    <div className="p-5 flex flex-col items-center">
       <h2 className="text-5xl text-center">Oferta para novos membros</h2>
 
-      <p className="text-xl mt-5">
+      <p className="text-xl mt-5 md:text-2xl text-center md:w-4/6">
         Inscreva-se agora e ganhe um dia de acesso gratuito para experimentar
         nosso espaço de coworking. Descubra como podemos ajudá-lo a alcançar
         seus objetivos profissionais!
       </p>
 
-      <div className="bg-slate-600 p-5 my-5 flex flex-col items-center">
+      <div className="bg-slate-600 p-5 my-5 flex flex-col items-center md:w-auto">
         <FaWhatsapp size='40' color="white" />
         <p className="text-3xl text-center text-white font-bold">
           Experimente gratuitamente agora
--- src/components/Satisfação.jsx
+++ src/components/Satisfação.jsx
@@ -3,13 +3,13 @@ import { FaFaceGrinWide } from "react-icons/fa6";
 
 const Satisfação = () => {
   return (
-    <div className="p-5 bg-black flex flex-col items-center">
+    <div className="p-10 bg-black flex flex-col items-center">
       <FaFaceGrinWide color="white" size={45} />
       <h2 className="text-white text-5xl text-center my-5">
         Garantia de Satisfação
       </h2>
 
-      <p className="text-white text-2xl text-center">
+      <p className="text-white text-2xl text-center md:w-4/6">
         Estamos confiantes de que nosso espaço atenderá às suas expectativas.
         Oferecemos uma garantia de satisfação, sem perguntas.
       </p>
